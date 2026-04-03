25c8f2661c2bb3e35bbc8bf72465246c16df232f
abumounir2226 <abumounir2226@gmail.com>
2026-04-03 09:38:18 +0000

    Fix type error in session

---
+++ next-auth.d.ts
@@ -0,0 +1,15 @@
+
+import "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    role: string;
+  }
+}
